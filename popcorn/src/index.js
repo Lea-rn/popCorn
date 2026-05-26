326f5975d7afc1770e64dbc0444f0431e8bedf3c
achrefmechergui <achreffcb123achref@gamil.com>
2026-05-26 17:56:02 +0100

    star component

--- popcorn/src/index.js
+++ popcorn/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
+// import "./App.css";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import StarRating from "./StarRating";
@@ -8,9 +9,14 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={8} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    {/* <StarRating maxRating={10} /> */}
+
+    <StarRating
+      maxRating={3}
+      color="pink"
+      messages={["khayib", "metwasset", "behi"]}
+    />
+
     {/* <App /> */}
   </React.StrictMode>,
 );
